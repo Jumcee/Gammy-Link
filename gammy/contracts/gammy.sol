3534dc46c4def4f83d56bf74951b97f82d43284c
Jumcee <futhmah456@gmail.com>
2023-12-20 16:50:22 +0100

    Gammy

--- gammy/contracts/gammy.sol
+++ gammy/contracts/gammy.sol
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract gammy {
-    struct user {
+contract Gammy {
+    struct User {
         bytes32 username;
         bytes32 password;
         uint256 balance;
     }
 
-    mapping(address => user) public user;
+    mapping(address => User) public users;
     mapping(bytes32 => address) public didToAddress;
-    mapping(bytes32 => bool) public usedDIDs;
 
-    event signUp(address indexed userAddress, bytes32 username, bytes32 did);
+    event SignUp(address indexed userAddress, bytes32 username, bytes32 did);
     event Login(address indexed userAddress);
     event Payment(address indexed from, address indexed to, uint256 amount);
 
@@ -21,30 +20,31 @@ contract gammy {
         bytes32 _password,
         bytes32 _did
     ) external {
-        //check if user aleady exist
+        // Check if user already exists
         require(
             users[msg.sender].username == bytes32(0),
             "User already exists"
         );
 
-        //create a new user
-        users[msg.sender] = User(_username, _password, msg.sender, _did);
-        emit UserSignedUp(msg.sender, _username, _did);
+        // Create a new user
+        users[msg.sender] = User(_username, _password, 0); // Assuming initial balance is 0
+        didToAddress[_did] = msg.sender;
+        emit SignUp(msg.sender, _username, _did);
     }
 
-    function Login(bytes32 _username, bytes32 _password) public {
+    function login(bytes32 _username, bytes32 _password) external {
         require(
             users[msg.sender].username == _username &&
                 users[msg.sender].password == _password,
             "Invalid credentials"
         );
-        emit userLoggedIn(msg.sender, _username);
+        emit Login(msg.sender);
     }
 
-    function addPayment(address_to, uint _amount) public {
-        require(users[msg.sender].balance => amount, "Insufficient fund");
-        user[msg.sender].balance -= _amount;
-        users[_to].balance =+ _amount;  
-        emit paymentSent(msg.sender, _to, _amount);
-        }
+    function addPayment(address _to, uint256 _amount) external {
+        require(users[msg.sender].balance >= _amount, "Insufficient funds");
+        users[msg.sender].balance -= _amount;
+        users[_to].balance += _amount;
+        emit Payment(msg.sender, _to, _amount);
+    }
 }
