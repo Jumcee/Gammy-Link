a8ba4fc2bbaad857bb7b56b4405251bc80bd2929
Jumcee <futhmah456@gmail.com>
2024-01-03 01:11:12 +0100

    updated

--- gammy/test/gammy.js
+++ gammy/test/gammy.js
@@ -1,64 +1,67 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
-describe("Gammy Contract", function () {
+describe('Gammy', function () {
+  let Gammy;
   let gammy;
+  let owner;
+  let user1;
+  let user2;
 
-  beforeEach(async () => {
-    const Gammy = await ethers.getContractFactory("Gammy");
+  beforeEach(async function () {
+    [owner, user1, user2] = await ethers.getSigners();
+
+    Gammy = await ethers.getContractFactory('Gammy');
     gammy = await Gammy.deploy();
-    await gammy.deployed();
+    await gammy.waitForDeployment();
   });
 
-  it("Should sign up a new user", async function () {
-    const [signer] = await ethers.getSigners();
+  it('should sign up a new user', async function () {
+    const username = ethers.utils.formatBytes32String('Alice');
+    const password = ethers.utils.formatBytes32String('password');
+    const did = ethers.utils.formatBytes32String('did123');
 
-    const username = ethers.utils.formatBytes32String("Alice");
-    const password = ethers.utils.formatBytes32String("123456");
-    const did = ethers.utils.formatBytes32String("someDID");
+    await expect(gammy.connect(user1).signUp(username, password, did))
+      .to.emit(gammy, 'SignUp')
+      .withArgs(user1.address, username, did);
 
-    await expect(gammy.signUp(username, password, did))
-      .to.emit(gammy, "SignUp")
-      .withArgs(signer.address, username, did);
-
-    const user = await gammy.users(signer.address);
+    const user = await gammy.users(user1.address);
     expect(user.username).to.equal(username);
     expect(user.password).to.equal(password);
-
-    const addressMappedToDID = await gammy.didToAddress(did);
-    expect(addressMappedToDID).to.equal(signer.address);
+    expect(user.balance).to.equal(0); // Initial balance should be 0
   });
 
-  it("Should allow a user to log in", async function () {
-    const [signer] = await ethers.getSigners();
+  it('should log in a user with valid credentials', async function () {
+    const username = ethers.utils.formatBytes32String('Alice');
+    const password = ethers.utils.formatBytes32String('password');
+    const did = ethers.utils.formatBytes32String('did123');
 
-    const username = ethers.utils.formatBytes32String("Alice");
-    const password = ethers.utils.formatBytes32String("123456");
-    const did = ethers.utils.formatBytes32String("someDID");
+    await gammy.connect(user1).signUp(username, password, did);
 
-    await gammy.signUp(username, password, did);
-
-    await expect(gammy.login(username, password))
-      .to.emit(gammy, "Login")
-      .withArgs(signer.address);
+    await expect(gammy.connect(user1).login(username, password))
+      .to.emit(gammy, 'Login')
+      .withArgs(user1.address);
   });
 
-  it("Should allow a user to add payment", async function () {
-    const [signer, receiver] = await ethers.getSigners();
+  it('should add payment between users', async function () {
+    const username1 = ethers.utils.formatBytes32String('Alice');
+    const username2 = ethers.utils.formatBytes32String('Bob');
+    const password = ethers.utils.formatBytes32String('password');
+    const did1 = ethers.utils.formatBytes32String('did123');
+    const did2 = ethers.utils.formatBytes32String('did456');
 
-    const amount = ethers.utils.parseEther("1");
+    await gammy.connect(user1).signUp(username1, password, did1);
+    await gammy.connect(user2).signUp(username2, password, did2);
 
-    await gammy.signUp(
-      ethers.utils.formatBytes32String("Alice"),
-      ethers.utils.formatBytes32String("123456"),
-      ethers.utils.formatBytes32String("someDID")
-    );
+    const amount = 100;
 
-    await gammy.addPayment(receiver.address, amount);
-    const senderBalance = await gammy.users(signer.address);
-    const receiverBalance = await gammy.users(receiver.address);
+    await expect(gammy.connect(user1).addPayment(user2.address, amount))
+      .to.emit(gammy, 'Payment')
+      .withArgs(user1.address, user2.address, amount);
 
-    expect(senderBalance.balance).to.equal(0);
-    expect(receiverBalance.balance).to.equal(amount);
+    const user1Balance = await gammy.users(user1.address);
+    const user2Balance = await gammy.users(user2.address);
+    expect(user1Balance.balance).to.equal(0); // Assuming initial balance was 0
+    expect(user2Balance.balance).to.equal(amount);
   });
 });
